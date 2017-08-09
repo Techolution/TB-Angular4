75abebccec5f280eeb288bce527d1a7cd1e3be7b
originalusrnm <jshornbuckle@gmail.com>
2017-08-09 15:31:16 -0400

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
-const routes: Routes = [
-  { path: '', component: DashboardComponent },
-  { path: 'employees', component: EmployeeListComponent },
-  { path: '**', redirectTo: '' }
-];
 
 @NgModule({
   declarations: [
@@ -21,8 +14,7 @@ const routes: Routes = [
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule,
-    RouterModule.forRoot(routes)
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
