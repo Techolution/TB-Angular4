e39a4af302030557e70ebbdda84226779a30a49a
originalusrnm <jshornbuckle@gmail.com>
2017-08-09 15:24:25 -0400

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    EmployeeFormComponent,
+    EmployeeDetailComponent,
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
