0efb7b114395802bcb984029008ac70231d64b6b
originalusrnm <jshornbuckle@gmail.com>
2017-08-13 11:59:44 -0400

    Update

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('tb-angular4 App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Component Unit Test!');
   });
 });
